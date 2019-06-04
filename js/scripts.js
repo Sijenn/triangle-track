5422aa147d2435586b96847a4f6196537f58214b
jenny <jenniffer>
2019-06-04 10:24:45 +0300

    added another alert function

--- js/scripts.js
+++ js/scripts.js
@@ -5,26 +5,30 @@ function tracker(){
     
    
     var result=document.querySelector('#response')
-    
-    if (sideA<=0 && sideB<=0 && sideC<=0)
+    if ( first <=0 || second <=0 || third <= 0) {
+        alert("you have entered wrong values")
+    }
+     else if (sideA<=0 && sideB<=0 && sideC<=0)
     {
         result.innerHTML=(' <h2>Value Cannot be less than or equals to zero </h2> <br> <img src="images/tenor.gif" alt="emoji">');
         alert("values cannot make a triangle")
-}
-    else if
+
+    }else if
     (sideA===sideB && sideB===sideC ){
     result.innerHTML=(' <h2>Equilateral triangle</h2> <br> <img src="images/equilateralTriangle.gif" alt="equilateral">');
     alert("Equilateral triangle")
+
+
+    }else if((sideA+sideB)<=sideC || (sideA+sideC)<=sideB || (sideB+sideC)<=sideA){
+        result.innerHTML=('These values do not make a triangle');
+        alert("values do not make a triangle")
+
     }else if(sideA===sideB ||sideB===sideC || sideA===sideC){
         result.innerHTML=(' <h2>Isoseles triangle</h2> <br> <img src="images/isosceles.jpg" alt="equilateral">');
         alert("Isosceles triangle")
-    }
 
-    else if((sideA+sideB)<=sideC || (sideA+sideB)<=sideC || (sideB+sideC)<=sideA){
-    result.innerHTML=('These values do not make a triangle');
-    alert("values do not make a triangle")
-}
-    else if(sideC + sideB > sideA || sideA + sideC > sideB || sideA + sideB > sideC){
+
+    }else if(sideC + sideB > sideA || sideA + sideC > sideB || sideA + sideB > sideC){
         result.innerHTML=(' <h2>Scalene triangle</h2> <br> <img src="images/scaleneTriangle.gif" alt="equilateral">');
         alert("scalene triangle")
     }
